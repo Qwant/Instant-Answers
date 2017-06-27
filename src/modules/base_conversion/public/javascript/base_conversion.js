c8b7970477e04aeb43c5d1b4c523e19e717e1f7d
j.mortier <j.mortier@qwant.com>
2017-06-27 14:36:59 +0200

    Add IA base conversion

---
+++ src/modules/base_conversion/public/javascript/base_conversion.js
@@ -0,0 +1,25 @@
+/**
+ * This is your main script file. Please refer to the documentation for more information.
+ */
+
+var IARuntime = function() {
+    function Base_conversion (iaData) {
+        // constructor
+    }
+
+    /**
+     * runs at runtime
+     */
+    Base_conversion.prototype.run = function() {
+        // function that's gonna run at runtime
+    };
+
+    /**
+     * runs upon exit
+     */
+    Base_conversion.prototype.stop = function() {
+        // function that's gonna run upon exit
+    };
+
+    return Base_conversion;
+}();
