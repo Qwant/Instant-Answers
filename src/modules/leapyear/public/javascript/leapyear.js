b2ef00ad6186425151187ba311832e47a5f8466a
j.mortier <j.mortier@qwant.com>
2017-06-27 09:53:32 +0200

    Start leapyear

---
+++ src/modules/leapyear/public/javascript/leapyear.js
@@ -0,0 +1,25 @@
+/**
+ * This is your main script file. Please refer to the documentation for more information.
+ */
+
+var IARuntime = function() {
+    function Leapyear (iaData) {
+        // constructor
+    }
+
+    /**
+     * runs at runtime
+     */
+    Leapyear.prototype.run = function() {
+        // function that's gonna run at runtime
+    };
+
+    /**
+     * runs upon exit
+     */
+    Leapyear.prototype.stop = function() {
+        // function that's gonna run upon exit
+    };
+
+    return Leapyear;
+}();
