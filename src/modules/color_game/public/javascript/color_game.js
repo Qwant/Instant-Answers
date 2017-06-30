19d295401158153987b3c237483ae8368be0ed3f
j.mortier <j.mortier@qwant.com>
2017-06-30 10:00:15 +0200

    Start ia

---
+++ src/modules/color_game/public/javascript/color_game.js
@@ -0,0 +1,25 @@
+/**
+ * This is your main script file. Please refer to the documentation for more information.
+ */
+
+var IARuntime = function() {
+    function Color_game (iaData) {
+        // constructor
+    }
+
+    /**
+     * runs at runtime
+     */
+    Color_game.prototype.run = function() {
+        // function that's gonna run at runtime
+    };
+
+    /**
+     * runs upon exit
+     */
+    Color_game.prototype.stop = function() {
+        // function that's gonna run upon exit
+    };
+
+    return Color_game;
+}();
