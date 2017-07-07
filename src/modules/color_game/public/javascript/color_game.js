a085ee1dbab67533c35359cde4d1bd3d9dc6c2fe
j.mortier <j.mortier@qwant.com>
2017-07-07 11:53:46 +0200

    Convert to dynamic

--- src/modules/color_game/public/javascript/color_game.js
+++ src/modules/color_game/public/javascript/color_game.js
@@ -11,7 +11,41 @@ var IARuntime = function() {
      * runs at runtime
      */
     Color_game.prototype.run = function() {
-        this.game();
+        var play = document.getElementById('start');
+        var can = document.getElementById("game");
+        var cross = document.getElementById("cross");
+        var elem = document.getElementById("background_games");
+        var state = 0; // 0 = little interface 1= big interface
+        var scope = this;
+        var idInterval = 0;
+        can.style.display = "none";
+        cross.style.display = "none";
+        play.addEventListener("click", function(){
+            if (state === 0){
+                elem.style.height = "800px";
+                setTimeout(function(){
+                    can.style.display = "block";
+                    cross.style.display = "block";
+                }, 800);
+                play.style.cursor = "default";
+                play.style.display = "none";
+                idInterval = scope.game();
+                state = 1;
+            }
+        });
+        cross.addEventListener("click", function(){
+            if(state === 1){
+                elem.style.height = "200px";
+                can.style.display = "none";
+                cross.style.display = "none";
+                setTimeout(function(){
+                    play.style.cursor = "pointer";
+                }, 800);
+                state = 0;
+                play.style.display = "block";
+                clearInterval(idInterval);
+            }
+        })
     };
 
     /**
@@ -22,12 +56,11 @@ var IARuntime = function() {
     };
 
     Color_game.prototype.game = function() {
-        var canvas = document.getElementById("colorgame");
+        var canvas = document.getElementById("mycanvas");
         var ctx = canvas.getContext("2d");
-        canvas.height = 200;
+        canvas.height = 600;
         canvas.width = 800;
-        var grow = false;
-        var screen = -1;
+        var screen = 0;
         var x = 0;
         var y = 0;
         var click = false;
@@ -40,8 +73,6 @@ var IARuntime = function() {
         var start;
         var end;
         var diff;
-        var idLauncher;
-        var idStart;
 
         window.addEventListener('mousemove', function (e) {
             x = e.pageX - canvas.offsetLeft;
@@ -76,29 +107,6 @@ var IARuntime = function() {
 
         function rgb(r, g, b){
             return "rgb("+r+","+g+","+b+")";
-        }
-
-        function drawLauncher() {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.beginPath();
-            ctx.fillStyle = rgb(0, 0, 0);
-            var textWidth = ctx.measureText("Play!").width;
-            ctx.font = "20px Arial";
-            ctx.fillText("Play!", canvas.width / 2 - textWidth / 2, canvas.height / 2);
-            ctx.closePath();
-            if (x >= canvas.width / 3 && x < 2 * canvas.width / 3 && y >= canvas.height / 3 && y < 2 * canvas.height / 3 && click) {
-                grow = true;
-                click = false;
-            }
-            if (grow && canvas.height < 600) {
-                canvas.height += 10;
-            }
-            else if (grow) {
-                screen = 0;
-                clearInterval(idLauncher);
-                idStart = setInterval(draw, 10);
-                grow = false;
-            }
         }
 
         function drawStart() {
@@ -189,91 +197,65 @@ var IARuntime = function() {
             ctx.closePath();
         }
 
-        function drawClose() {
-            ctx.beginPath();
-            ctx.fillStyle = "#000000";
-            ctx.font = "20px Arial";
-            var textWidth = ctx.measureText("Close").width;
-            ctx.fillText("Close", 760 - textWidth / 2, 30);
-            ctx.closePath();
-        }
-
         function draw() {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            drawClose();
-            if (click && x >= 760 && x < 800 && y >= 0 && y < 50) {
-                grow = true;
-                click = false;
-            }
-            if (grow) {
-                if (canvas.height > 200) {
-                    canvas.height -= 10;
-                }
-                else {
-                    clearInterval(idStart);
-                    screen     = -1;
-                    idLauncher = setInterval(drawLauncher, 10);
-                    grow       = false;
+            if (screen === 0) {
+                drawStart();
+                if (x >= canvas.width / 3 && x < 2 * canvas.width / 3 && y >= canvas.height / 3 && y < 2 * canvas.height / 3 && click) {
+                    row    = 2;
+                    column = 2;
+                    level  = 1;
+                    pos    = Math.trunc(Math.random() * 10000) % (row * column);
+                    color  = randomColor();
+                    screen = (screen + 1) % 3;
+                    lumi   = Math.trunc(Math.random() * 10000) % 2;
+                    start  = new Date();
                 }
             }
-            else {
-                if (screen === 0) {
-                    drawStart();
-                    if (x >= canvas.width / 3 && x < 2 * canvas.width / 3 && y >= canvas.height / 3 && y < 2 * canvas.height / 3 && click) {
-                        row    = 2;
-                        column = 2;
-                        level  = 1;
-                        pos    = Math.trunc(Math.random() * 10000) % (row * column);
-                        color  = randomColor();
+            else if (screen === 1) {
+                drawGame();
+                drawCadri();
+                drawChrono();
+                drawReset();
+                if ((x >= (pos % column) * Math.trunc(canvas.height / column) && x < (1 + (pos % column)) * Math.trunc(canvas.height / column)) &&
+                    (y >= Math.trunc(pos / column) * Math.trunc(canvas.height / row) && y < (1 + Math.trunc(pos / column)) * Math.trunc(canvas.height / row)) && click) {
+                    ++level;
+                    if (row < 14 && column < 14) {
+                        ++row;
+                        ++column;
+                    }
+                    pos   = Math.trunc(Math.random() * 10000) % (row * column);
+                    color = randomColor();
+                    lumi  = Math.trunc(Math.random() * 10000) % 2;
+                    if (level === 50) {
                         screen = (screen + 1) % 3;
-                        lumi   = Math.trunc(Math.random() * 10000) % 2;
-                        start  = new Date();
                     }
                 }
-                else if (screen === 1) {
-                    drawGame();
-                    drawCadri();
-                    drawChrono();
-                    drawReset();
-                    if ((x >= (pos % column) * Math.trunc(canvas.height / column) && x < (1 + (pos % column)) * Math.trunc(canvas.height / column)) &&
-                        (y >= Math.trunc(pos / column) * Math.trunc(canvas.height / row) && y < (1 + Math.trunc(pos / column)) * Math.trunc(canvas.height / row)) && click) {
-                        ++level;
-                        if (row < 14 && column < 14) {
-                            ++row;
-                            ++column;
-                        }
-                        pos   = Math.trunc(Math.random() * 10000) % (row * column);
-                        color = randomColor();
-                        lumi  = Math.trunc(Math.random() * 10000) % 2;
-                        if (level === 50) {
-                            screen = (screen + 1) % 3;
-                        }
-                    }
-                    if (x >= 600 && x < 800 && y >= 300 && y < 600 && click) {
-                        row    = 2;
-                        column = 2;
-                        level  = 1;
-                        pos    = Math.trunc(Math.random() * 10000) % (row * column);
-                        color  = randomColor();
-                        lumi   = Math.trunc(Math.random() * 10000) % 2;
-                        start  = new Date();
-                    }
+                if (x >= 600 && x < 800 && y >= 300 && y < 600 && click) {
+                    row    = 2;
+                    column = 2;
+                    level  = 1;
+                    pos    = Math.trunc(Math.random() * 10000) % (row * column);
+                    color  = randomColor();
+                    lumi   = Math.trunc(Math.random() * 10000) % 2;
+                    start  = new Date();
                 }
-                else if (screen === 2) {
-                    drawEnd();
-                    if (x >= canvas.width / 3 && x < 2 * canvas.width / 3 && y >= canvas.height / 3 && y < 2 * canvas.height / 3 && click) {
-                        row    = 2;
-                        column = 2;
-                        pos    = Math.trunc(Math.random() * 10000) % (row * column);
-                        color  = randomColor();
-                        screen = (screen + 1) % 3;
-                        lumi   = Math.trunc(Math.random() * 10000) % 2;
-                    }
+            }
+            else if (screen === 2) {
+                drawEnd();
+                if (x >= canvas.width / 3 && x < 2 * canvas.width / 3 && y >= canvas.height / 3 && y < 2 * canvas.height / 3 && click) {
+                    row    = 2;
+                    column = 2;
+                    pos    = Math.trunc(Math.random() * 10000) % (row * column);
+                    color  = randomColor();
+                    screen = (screen + 1) % 3;
+                    lumi   = Math.trunc(Math.random() * 10000) % 2;
                 }
             }
             click = false;
         }
-        idLauncher = setInterval(drawLauncher, 10);
+        var idInteverval = setInterval(draw, 10);
+        return (idInteverval);
     };
 
     return Color_game;
