11fb547c273e349bca5a0602ba8deaae9fba8e7f
j.mortier <j.mortier@qwant.com>
2017-07-11 11:10:27 +0200

    Create IA

---
+++ src/modules/scroller/public/javascript/scroller.js
@@ -0,0 +1,25 @@
+/**
+ * This is your main script file. Please refer to the documentation for more information.
+ */
+
+var IARuntime = function() {
+    function Scroller (iaData) {
+        // constructor
+    }
+
+    /**
+     * runs at runtime
+     */
+    Scroller.prototype.run = function() {
+        // function that's gonna run at runtime
+    };
+
+    /**
+     * runs upon exit
+     */
+    Scroller.prototype.stop = function() {
+        // function that's gonna run upon exit
+    };
+
+    return Scroller;
+}();
