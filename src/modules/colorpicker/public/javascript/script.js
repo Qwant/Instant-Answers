edc85ec0645cb031525346ae7eafbda788febdb4
v.vidal <v.vidal@qwant.com>
2017-07-21 10:00:13 +0200

    IA-COLORPICKER create IA

---
+++ src/modules/colorpicker/public/javascript/script.js
@@ -0,0 +1,25 @@
+/**
+ * This is your main script file. Please refer to the documentation for more information.
+ */
+
+var IARuntime = function() {
+    function Colorpicker (iaData) {
+        // constructor
+    }
+
+    /**
+     * runs at runtime
+     */
+    Colorpicker.prototype.run = function() {
+        // function that's gonna run at runtime
+    };
+
+    /**
+     * runs upon exit
+     */
+    Colorpicker.prototype.stop = function() {
+        // function that's gonna run upon exit
+    };
+
+    return Colorpicker;
+}();
