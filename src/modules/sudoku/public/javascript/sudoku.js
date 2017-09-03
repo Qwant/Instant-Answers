96389794eac834d63da0dbcc1421899ebd0ff22e
Teuop <gfend@mail.com>
2017-09-04 00:43:52 +0200

    Just starting.

---
+++ src/modules/sudoku/public/javascript/sudoku.js
@@ -0,0 +1,25 @@
+/**
+ * This is your main script file. Please refer to the documentation for more information.
+ */
+
+var IARuntime = function() {
+    function Sudoku (iaData) {
+        // constructor
+    }
+
+    /**
+     * runs at runtime
+     */
+    Sudoku.prototype.run = function() {
+        // function that's gonna run at runtime
+    };
+
+    /**
+     * runs upon exit
+     */
+    Sudoku.prototype.stop = function() {
+        // function that's gonna run upon exit
+    };
+
+    return Sudoku;
+}();
