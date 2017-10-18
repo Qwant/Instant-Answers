a7a7ab4503f4ead8000eec8586b18e44e154d6e8
Florian Courgey <florian@floriancourgey.com>
2017-10-18 12:10:18 +0200

    first commit with "node bin/ia-new"

---
+++ src/modules/beautify/public/javascript/beautify.js
@@ -0,0 +1,25 @@
+/**
+ * This is your main script file. Please refer to the documentation for more information.
+ */
+
+var IARuntime = function() {
+    function Beautify (iaData) {
+        // constructor
+    }
+
+    /**
+     * runs at runtime
+     */
+    Beautify.prototype.run = function() {
+        // function that's gonna run at runtime
+    };
+
+    /**
+     * runs upon exit
+     */
+    Beautify.prototype.stop = function() {
+        // function that's gonna run upon exit
+    };
+
+    return Beautify;
+}();
