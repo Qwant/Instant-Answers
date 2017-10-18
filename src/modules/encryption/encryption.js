dd5aab9dac2241ca643c1fa8ae59d6cb789cc435
Florian Courgey <florian@floriancourgey.com>
2017-10-18 11:36:20 +0200

    fix tabs instead of whitespaces

--- src/modules/encryption/encryption.js
+++ src/modules/encryption/encryption.js
@@ -14,13 +14,13 @@ module.exports = {
 
     /**
      * (NEEDED)
-     *	This function uses 3 parameters :
-     *	- values : This is an array of values caught by regex.
-     *	For example, if you use the keyword "test" with the trigger "start", and you type "test working?",
+     *  This function uses 3 parameters :
+     *  - values : This is an array of values caught by regex.
+     *  For example, if you use the keyword "test" with the trigger "start", and you type "test working?",
      *  values would be like this :
-     *  	* values[0] = "test working?"
-     *  	* values[1] = "test"
-     *  	* values[2] = "working?"
+     *    * values[0] = "test working?"
+     *    * values[1] = "test"
+     *    * values[2] = "working?"
      *  But, if you use the trigger "strict", there will be only one value in this array (values[0] = "test working?")
      *  - proxyURL : If you need to call an external API, use the package "request" with proxyURL as value for
      *  "proxy" attribute (you can refer to weather IA to check how to use it properly)
@@ -33,7 +33,7 @@ module.exports = {
         var stringToHash = values[2];
 
         commonEntities.forEach(function(entity) {
-			stringToHash = stringToHash.replace(entity.html, entity.char);
+            stringToHash = stringToHash.replace(entity.html, entity.char);
         });
 
         var result = '';
@@ -75,7 +75,7 @@ module.exports = {
      * (NEEDED)
      * triggers : Depending on the trigger, the keyword needs to be placed at a specific point in the query.
      * It has 4 different values :
-     * 			start  : keyword + string
+     *          start  : keyword + string
      *          end    : string + keyword
      *          any    : string + keyword + string
      *          strict : perfect match with keyword
@@ -86,9 +86,9 @@ module.exports = {
     /**
      * (NEEDED)
      * flag : Only 3 flags allowed : (default : i)
-     * 			- g : global
-     * 			- m : multi-line
-     * 			- i : insensitive
+     *      - g : global
+     *      - m : multi-line
+     *      - i : insensitive
      */
 
     flag: "i",
