a149606c7436866d6c9defd670140ee76eb15dad
David Routhieau <rootio@protonmail.com>
2018-02-04 23:10:30 +0100

    Calculator fixed notation

--- src/modules/calculator/test/index.js
+++ src/modules/calculator/test/index.js
@@ -13,6 +13,28 @@ describe('Calculator', function() {
 		})
 	});
 
+	it('Respond with fixed notation (1 * 100000 = 100000)', function(done) {
+		let resultPromise = Calculator.getData(['1 * 100000']);
+		resultPromise.then((result) => {
+			assert.equal(result, 100000);
+			assert.notEqual(result.toString(), '1e+5');
+			done()
+		}).catch((error) => {
+			done(error)
+		})
+	});
+
+	it('Respond with no extra zero precision (1 * 1.10 = 1.1)', function(done) {
+		let resultPromise = Calculator.getData(['1 * 1.10']);
+		resultPromise.then((result) => {
+			assert.equal(result, 1.1);
+			assert.notEqual(result.toString(), '1.10');
+			done()
+		}).catch((error) => {
+			done(error)
+		})
+	});
+
 	it('Handling error', function(done) {
 		let resultPromise = Calculator.getData(['3****2']);
 		resultPromise.then((result) => {
