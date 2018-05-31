207a3d4dea374be166bae89ff7766e33f6ba9edc
fiksd <moi@larch.localdomain>
2018-05-31 22:20:17 +0000

    Added a base conversion tool to the calculator

--- src/modules/calculator/calculator.js
+++ src/modules/calculator/calculator.js
@@ -10,9 +10,9 @@ module.exports = {
      *	- values : This is an array of values caught by regex.
      *	For example, if you use the keyword "test" with the trigger "start", and you type "test working?",
      *  values would be like this :
-     *  	* values[0] = "test working?"
-     *  	* values[1] = "test"
-     *  	* values[2] = "working?"
+     *		* values[0] = "test working?"
+     *		* values[1] = "test"
+     *		* values[2] = "working?"
      *  But, if you use the trigger "strict", there will be only one value in this array (values[0] = "test working?")
      *  - proxyURL : If you need to call an external API, use the package "request" with proxyURL as value for
      *  "proxy" attribute (you can refer to weather IA to check how to use it properly)
@@ -21,9 +21,27 @@ module.exports = {
      */
 
     getData: function (values) {
+        var getNumberWithRadix = this.getNumberWithRadix;
         return new Promise(function (resolve, reject) {
             if(!isNaN(values[0])) {
                 reject("Don't allow function identity", {module : "calculator"});
+			} else if(/^[a-z]+\s+to/i.test(values[0])) {
+                // conversion expression: <base A> to <base B> <number>
+				var expression = /^(bin|hex|dec|oct)\s+to\s+(bin|hex|dec|oct)\s+([0-9a-f]+)$/i.exec(values[0]);
+				if(!expression) {
+					reject("This conversion is invalid", {module : "calculator"});
+                } else {
+                    var radices = { 'bin': 2, 'oct': 8, 'dec': 10, 'hex': 16 };
+                    var sourceRadix = radices[expression[1].toLowerCase()];
+                    var targetRadix = radices[expression[2].toLowerCase()];
+                    var queryString = getNumberWithRadix(expression[3], sourceRadix);
+                    var resultString = getNumberWithRadix(expression[3], sourceRadix, targetRadix);
+                    if(sourceRadix != targetRadix && queryString && resultString) {
+                        resolve({ query: queryString, result: resultString });
+                    } else {
+                        reject("This conversion is incoherent", {module : "calculator"});
+                    }
+				}
             } else {
                 values[0] = values[0].toLowerCase();
                 values[0] = values[0].replace("×","*");
@@ -63,13 +81,13 @@ module.exports = {
      * The keyword can be a regex. If you need help for your regex, use this https://regex101.com/#javascript
      */
 
-    keyword: "(\\+|\\-|\\*|\\/|×|÷|[0-9]|\\(|\\)|\\.|\\s|E|PI|PHI|cos|sin|sqrt|log|tan|exp|\\^)*",
+    keyword: "(\\+|\\-|\\*|\\/|×|÷|[0-9a-f]|\\(|\\)|\\.|\\s|E|PI|PHI|cos|sin|sqrt|log|tan|exp|\\^|to|bin|hex|dec|oct)*",
 
     /**
      * (NEEDED)
      * triggers : Depending on the trigger, the keyword needs to be placed at a specific point in the query.
      * It has 4 different values :
-     * 			start  : keyword + string
+     *          start  : keyword + string
      *          end    : string + keyword
      *          any    : string + keyword + string
      *          strict : perfect match with keyword
@@ -80,9 +98,9 @@ module.exports = {
     /**
      * (NEEDED)
      * flag : Only 3 flags allowed : (default : i)
-     * 			- g : global
-     * 			- m : multi-line
-     * 			- i : insensitive
+     *			- g : global
+     *			- m : multi-line
+     *			- i : insensitive
      */
 
     flag: "i",
@@ -99,5 +117,32 @@ module.exports = {
      * cache : Duration of the data cached (in seconds)
      */
 
-    cache: 200
+    cache: 200,
+
+    /**
+     * converts the string representation of an integer from one base to another,
+     * appending the radix as subscript
+     *
+     *   number : Input number as a string
+     *   sourceRadix : Radix of the input number
+     *   targetRadix : Destination radix (can be omitted for no conversion)
+     */
+
+    getNumberWithRadix: function (number, sourceRadix, targetRadix = null) {
+        if(targetRadix) {
+            number = parseInt(number, sourceRadix);
+            if(!Number.isFinite(number)) {
+                return null;
+            }
+            number = number.toString(targetRadix);
+        } else {
+            targetRadix = sourceRadix;
+        }
+        var radix = '';
+        while(targetRadix > 0) {
+            radix = String.fromCharCode(0x2080 + targetRadix%10)+radix;
+            targetRadix = Math.floor(targetRadix / 10);
+        }
+        return number+radix;
+    }
 };
